9c8a2c0eb37bd5194c84b2dca79e25d5a4346dc1
KYapp <kukiyomenaisapp@gmail.com>
2023-04-25 18:15:18 +0900

    BlogReaderを関数に分離

--- BlogSystem/BlogSystem/BlogReader.cs
+++ BlogSystem/BlogSystem/BlogReader.cs
@@ -1,11 +1,39 @@
+using System.Collections.ObjectModel;
+
 namespace UnityCup.BlogSystem;
 
 public class BlogReader
 {
-    public readonly string defaultPath;
+    public const string blogMarkdownName = "index.md";
+    public readonly string blogPath;
+    public ReadOnlyCollection<BlogPage> pages { get; private set; }
 
-    public BlogReader(string defaultPath)
+    public BlogReader(string blogPath)
     {
-        this.defaultPath = defaultPath;
+        this.blogPath = blogPath;
+        pages = new(new List<BlogPage>());
+    }
+
+    public void Read()
+    {
+        pages = new(
+            Directory.GetDirectories(blogPath, "*", SearchOption.TopDirectoryOnly)
+                .Select(path => new DirectoryInfo(path))
+                .Select(directoryInfo =>
+                    (
+                        pageName: directoryInfo.Name,
+                        fileInfo: directoryInfo.GetFiles()
+                            .Where(fileInfo => fileInfo.Name == blogMarkdownName)
+                            .First()
+                    )
+                )
+                .Select(data =>
+                    new BlogPage(
+                        data.pageName,
+                        data.fileInfo.OpenText().ReadToEnd()
+                    )
+                )
+                .ToArray()
+        );
     }
 }
