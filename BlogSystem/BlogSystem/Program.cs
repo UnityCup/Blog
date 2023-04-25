0811c46afd0eb5b91fb632ac0023e9a5a570a36f
KYapp <kukiyomenaisapp@gmail.com>
2023-04-25 15:03:34 +0900

    indexを追加してみた

--- BlogSystem/BlogSystem/Program.cs
+++ BlogSystem/BlogSystem/Program.cs
@@ -1,4 +1,7 @@
 ﻿const string path = "Build";
 
 if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+
 File.WriteAllText(Path.Join(path, @"test2.html"), "HOGEEEEEEEEEEEEEEEEE");
+File.WriteAllText(Path.Join(path, @"index.html"), "this is index.");
