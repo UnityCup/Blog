bd708457a38d6fcde75e783602fbd503741f2f85
KYapp <kukiyomenaisapp@gmail.com>
2023-04-25 15:19:28 +0900

    読み込みができるかのテスト

--- BlogSystem/BlogSystem/Program.cs
+++ BlogSystem/BlogSystem/Program.cs
@@ -2,6 +2,7 @@
 
 if (!Directory.Exists(path)) Directory.CreateDirectory(path);
 
+Console.WriteLine(File.ReadAllText("Blog/test.html"));
 
 File.WriteAllText(Path.Join(path, @"test2.html"), "HOGEEEEEEEEEEEEEEEEE");
 File.WriteAllText(Path.Join(path, @"index.html"), "this is index.");
