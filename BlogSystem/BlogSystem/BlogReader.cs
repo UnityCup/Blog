5b247313887e5697a7efcb93c6c3622fe6ab5251
KYapp <kukiyomenaisapp@gmail.com>
2023-04-25 15:37:37 +0900

    BlogReaderクラスを作成

---
+++ BlogSystem/BlogSystem/BlogReader.cs
@@ -0,0 +1,11 @@
+namespace UnityCup.BlogSystem;
+
+public class BlogReader
+{
+    public readonly string defaultPath;
+
+    public BlogReader(string defaultPath)
+    {
+        this.defaultPath = defaultPath;
+    }
+}
