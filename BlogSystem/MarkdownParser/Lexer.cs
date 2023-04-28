096472b8ac011cabe2e4ff113f08f615f20aac99
Juha <kukiyomenaisapp@gmail.com>
2023-04-28 13:13:46 +0900

    CurrentChar系

--- BlogSystem/MarkdownParser/Lexer.cs
+++ BlogSystem/MarkdownParser/Lexer.cs
@@ -3,13 +3,32 @@
 public class Lexer
 {
     public readonly string source;
-    public int currentPosition { get; }
-    public char currentChar => source[currentPosition];
-    public char nextChar => source[currentPosition + 1];
+    public int currentPosition { get; private set; }
+    public char currentChar
+    {
+        get
+        {
+            if (source.Length <= currentPosition) return ' ';
+            return source[currentPosition];
+        }
+    }
+    public char nextChar
+    {
+        get
+        {
+            if (source.Length <= currentPosition + 1) return ' ';
+            return source[currentPosition + 1];
+        }
+    }
 
     public Lexer(string source)
     {
         this.source = source;
+    }
+
+    public void NextChar()
+    {
+        currentPosition++;
     }
 
     public Token NextToken()
