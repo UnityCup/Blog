0bb88f6f72af525a70a71a1c4744011034668cb4
KYapp <kukiyomenaisapp@gmail.com>
2023-04-28 18:32:26 +0900

    H1をパース出来るようにした

--- BlogSystem/MarkdownParser/Parser.cs
+++ BlogSystem/MarkdownParser/Parser.cs
@@ -4,19 +4,58 @@ namespace UnityCup.MarkdownParser;
 
 public class Parser
 {
+    public Token currentToken { get; private set; }
+    public Token nextToken { get; private set; }
     private readonly Lexer lexer;
 
     public Parser(Lexer lexer)
     {
         this.lexer = lexer;
+        nextToken = lexer.NextToken();
+        currentToken = lexer.NextToken();
     }
 
     public Root Parse()
     {
         List<IStatement> statements = new List<IStatement>();
 
-
+        while (currentToken.type != TokenType.EOF)
+        {
+            IStatement? statement = ParseStatement();
+            if (statement is not null)
+            {
+                statements.Add(statement);
+            }
+            else
+            {
+                ReadToken();
+            }
+        }
 
         return new Root(statements);
+    }
+
+    private void ReadToken()
+    {
+        currentToken = nextToken;
+        nextToken = lexer.NextToken();
+    }
+
+    private IStatement? ParseStatement()
+    {
+        switch (currentToken.type)
+        {
+            case TokenType.Headline1:
+                return ParseHeadLine1Statement();
+            default:
+                return null;
+        }
+    }
+
+    private HeadLine1Statement? ParseHeadLine1Statement()
+    {
+        HeadLine1Statement statement = new HeadLine1Statement(currentToken.literal);
+        ReadToken();
+        return statement;
     }
 }
