214abd0faca89cab6b6ca4e1c90ad88e5e72b49a
Juha <kukiyomenaisapp@gmail.com>
2023-04-28 14:43:00 +0900

    ParserTestを追加

---
+++ BlogSystem/ParserTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using UnityCup.MarkdownParser;
+
+namespace UnityCup.ParserTest;
+
+public class Program
+{
+    private readonly Lexer lexer;
+
+    public Program()
+    {
+        this.lexer = new Lexer(@"aaaaaaa
+#afwfa
+#kkkkkk
+## feffeffe
+fafwafeef");
+
+        Token token = lexer.NextToken();
+        while (true)
+        {
+            Console.WriteLine($"{token.literal} : {token.type}");
+            if (token.type == TokenType.EOF) return;
+            token = lexer.NextToken();
+        }
+    }
+
+    public static void Main(string[] args) => new Program();
+}
